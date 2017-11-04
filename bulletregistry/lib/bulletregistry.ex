2a43318520e6d551f6e41bdf3ef6700f927d8895
Luke Wood <lukewoodcs@gmail.com>
2017-11-04 17:07:06 -0400

    genserverwork

--- bulletregistry/lib/bulletregistry.ex
+++ bulletregistry/lib/bulletregistry.ex
@@ -1,11 +1,11 @@
 defmodule BulletRegistry do
 
-  defdelegate tick(),                       to: BulletRegistry.Client
+  defdelegate tick(pid\\BulletRegistry),                       to: BulletRegistry.Client
   defdelegate add_bullet(x, y, direction),  to: BulletRegistry.Client
-  defdelegate peek(),                       to: BulletRegistry.Client
+  defdelegate peek(pid\\BulletRegistry),                       to: BulletRegistry.Client
 
   defdelegate new_registry(),               to: BulletRegistry.Supervisor
-  
+
   def start(_type, _opts) do
     BulletRegistry.Supervisor.start_child_registry
   end
--- bulletregistry/test/bulletregistry_server_test.exs
+++ bulletregistry/test/bulletregistry_server_test.exs
@@ -3,14 +3,16 @@ defmodule ImplTest.Server do
 
   test "the server boots into a new state" do
     pid = BulletRegistry.new_registry
+    IO.inspect pid
     BulletRegistry.tick(pid)
     assert Enum.count((BulletRegistry.peek(pid)).bullets) == 0
   end
 
   test "bullets can be added to the server" do
-    BulletRegistry.tick()
-    BulletRegistry.add_bullet(0, 0, :up)
-    BulletRegistry.tick()
+    pid = BulletRegistry.new_registry
+    BulletRegistry.tick(pid)
+    BulletRegistry.add_bullet(0, 0, :up, pid)
+    BulletRegistry.tick(pid)
     assert Enum.count((BulletRegistry.peek).bullets) == 1
   end
 
