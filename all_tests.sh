92a406f41a1f7e5385e322eafcc19ba4f7ee0e18
Luke Wood <lukewoodcs@gmail.com>
2017-11-05 18:18:18 -0500

    Sweet - let's start refactoring bullet server to not need a key value registry.  We can just handle the state with a genserver state tuple instead

--- all_tests.sh
+++ all_tests.sh
@@ -1,6 +1,8 @@
+rm tests.out
 for x in bulletserver gameserver keyval lobbyserver playerserver;
 do
 	cd $x
+	echo "######### Testing $x ########" >> ../tests.out
 	mix test >> ../tests.out
 	cd ..
 done
