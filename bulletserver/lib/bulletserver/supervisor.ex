e9fb2ae8e50ee4d97299c51aea17f6fc68d29a28
Luke Wood <lukewoodcs@gmail.com>
2017-11-05 18:11:06 -0500

    bullet server done

--- bulletserver/lib/bulletserver/supervisor.ex
+++ bulletserver/lib/bulletserver/supervisor.ex
@@ -1,15 +1,9 @@
 defmodule BulletServer.Supervisor do
 
-  alias KeyVal, as: ChildrenRegistry
-
-
-
   def new_registry do
     {:ok, par_pid} = GenServer.start_link(BulletServer.Server, BulletServer.Impl.new_registry)
     {:ok, child_pid} = BulletServer.ChangeList.start_link
-    ChildrenRegistry.set(par_pid, child_pid)
+    KeyVal.set(par_pid, child_pid)
     par_pid
   end
-
-  BulletServer.ChangeList.start_link
 end
