af104e8011ba21486e3ca15a713edab6f8ce18c2
Luke Wood <lukewoodcs@gmail.com>
2017-11-05 18:18:55 -0500

    Extra hash mark in shell script - fixing this to cure someone's ocd someone.  You're welcome friend :)

--- all_tests.sh
+++ all_tests.sh
@@ -2,7 +2,7 @@ rm tests.out
 for x in bulletserver gameserver keyval lobbyserver playerserver;
 do
 	cd $x
-	echo "######### Testing $x ########" >> ../tests.out
+	echo "######## Testing $x ########" >> ../tests.out
 	mix test >> ../tests.out
 	cd ..
 done
