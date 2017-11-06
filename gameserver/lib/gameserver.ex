0f2f84e4b91e59b2ecacfd3742d071a2e8be567e
Luke Wood <lukewoodcs@gmail.com>
2017-11-06 15:37:40 -0600

    removed gameserver sadly

--- gameserver/lib/gameserver.ex
+++
@@ -1,9 +0,0 @@
-defmodule GameServer do
-
-  defdelegate tick(pid),                       to: GameServer.Client
-  defdelegate peek(pid),                       to: GameServer.Client
-  defdelegate player_input(pid, from, action), to: GameServer.Client
-  defdelegate add_player(pid, name),           to: GameServer.Client
-  defdelegate new_server,                      to: GameServer.Supervisor
-
-end
--- gameserver/lib/gameserver/children.ex
+++
@@ -1,6 +0,0 @@
-defmodule GameServer.Children do
-  defstruct [
-    bullet_server: nil,
-    player_server: nil
-  ]
-end
