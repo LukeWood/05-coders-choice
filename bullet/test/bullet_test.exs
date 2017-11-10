2d57b804c1c076bdea723a95e8c0cb48d8aae80c
Luke Wood <lukewoodcs@gmail.com>
2017-11-09 20:23:07 -0600

    Tests

--- bullet/test/bullet_test.exs
+++ bullet/test/bullet_test.exs
@@ -1,15 +1,24 @@
 defmodule BulletTest do
   use ExUnit.Case
-  
-  test "Bullet can be spawned" do
+
+
+
+  test "can be spawned" do
     {:ok, pid } = Bullet.new
     assert pid != nil
   end
 
-  test "Bullet moves when tick is called" do
+  test "moves when tick is called" do
     {:ok, pid} = Bullet.new
     Observable.emit(Clock, {:tick})
     assert Bullet.peek(pid) |> Map.get(:x) == -1
+  end
+
+  test "be created to mirror a players" do
+    # nil world pid
+    {:ok, pid} = Bullet.new(%{x: 100, y: 100, direction: :right}, nil)
+    Observable.emit(Clock, {:tick})
+    assert Bullet.peek(pid) |> Map.get(:x) == 101
   end
 
 end
