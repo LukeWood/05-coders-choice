af6bd841e5f6da1cbbe246071565edcf641aa5aa
Luke Wood <lukewoodcs@gmail.com>
2017-11-10 23:51:24 -0600

    Player overhaul

--- player/lib/player/supervisor.ex
+++ player/lib/player/supervisor.ex
@@ -1,4 +1,22 @@
 defmodule Player.Supervisor do
+  use Supervisor
 
+  def start(_opts, _args) do
+    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def new do
+    Supervisor.start_child(__MODULE__, [])
+  end
+
+  def init(:ok) do
+    children =[
+      worker(Player.Server, [], restart: :temporary)
+    ]
+    opts = [
+      strategy: :simple_one_for_one
+    ]
+    supervise(children, opts)
+  end
 
 end
