f1a3c8b76ff1829d89d2d6d74ca8fa9707cae8e4
Luke Wood <lukewoodcs@gmail.com>
2017-11-11 00:06:12 -0600

    player tests work

--- player/test/player_test.exs
+++ player/test/player_test.exs
@@ -1,16 +1,30 @@
 defmodule PlayerTest do
   use ExUnit.Case
-
   import Player
+
+  def tick() do
+    Observable.emit(Clock, {:tick})
+  end
 
   test "Create a player" do
     {:ok, pid} = new()
     assert pid != nil
   end
 
-  test "Actions update our player" do
-    {:ok, pid} = new()
-    action(pid, :left)
-    assert peek(pid) |> Map.get(:direction) == :left
+  describe "Players will " do
+    test "respond to updates" do
+      {:ok, pid} = new()
+      action(pid, :left)
+      assert peek(pid) |> Map.get(:direction) == :left
+    end
+
+    test "move" do
+      {:ok, pid} = new()
+      action(pid, :left)
+      tick()
+      assert peek(pid) |> Map.get(:x) == -1
+    end
+    
   end
+
 end
