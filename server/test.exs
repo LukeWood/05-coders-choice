0560d5b830395de64da32b015f271b89c57e5721
Luke Wood <lukewoodcs@gmail.com>
2017-11-13 13:51:50 -0600

    game server

---
+++ server/test.exs
@@ -0,0 +1,10 @@
+{:ok, world} = World.new
+{:ok, p1} = Player.new world
+{:ok, _p2} = Player.new world
+import Player
+action(p1, :left)
+:timer.sleep(1000)
+action(p1, :right)
+action(p1, :stop)
+action(p1, :shoot)
+:timer.sleep(1300)
