fff0147dec8604d5add9eb784dfab950ca1c15a1
Luke Wood <lukewoodcs@gmail.com>
2017-11-13 16:06:30 -0600

    sweet

--- integration/test/server_test.exs
+++ integration/test/server_test.exs
@@ -1,5 +1,5 @@
 defmodule IntegrationTest do
-  
+
   use ExUnit.Case
   test "Player 1 lives, Player 2 dies" do
     {:ok, world} = World.new
@@ -13,6 +13,23 @@ defmodule IntegrationTest do
     action(p1, :shoot)
     :timer.sleep(1300)
 
+    assert Process.alive?(p1)
+    assert !Process.alive?(p2)
+  end
+
+  test "Dead men shoot no bullets" do
+    {:ok, world} = World.new
+    {:ok, p1} = Player.new world
+    {:ok, p2} = Player.new world
+    import Player
+    action(p1, :left)
+    :timer.sleep(1000)
+    action(p1, :right)
+    action(p1, :stop)
+    action(p1, :shoot)
+    :timer.sleep(1300)
+    action(p2, :shoot)
+    :timer.sleep(1300)
     assert Process.alive?(p1)
     assert !Process.alive?(p2)
   end
