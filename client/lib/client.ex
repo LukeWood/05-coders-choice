a6867ffcaa51476f80e4f9c69d234b317e049530
Luke Wood <lukewoodcs@gmail.com>
2017-11-24 11:12:53 -0800

    client test

--- client/lib/client.ex
+++ client/lib/client.ex
@@ -1,3 +1,18 @@
 defmodule Client do
 
+  def start() do
+    {:ok, pid} = World.new
+    Agent.start_link(fn -> pid end, name: __MODULE__)
+  end
+
+  def get_state do
+    pid = Agent.get(__MODULE__, fn pid -> pid end)
+    players = World.get_players(pid)
+    bullets = World.get_bullets(pid)
+    %{
+      bullets: bullets,
+      players: players
+    }
+  end
+
 end
--- client/lib/client_web.ex
+++ client/lib/client_web.ex
@@ -41,9 +41,6 @@ defmodule ClientWeb do
     end
   end
 
-  @doc """
-  When used, dispatch to the appropriate controller/view/etc.
-  """
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
   end
