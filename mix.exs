8116405ea8b4648455bbdf0bd65a3d93646fe62e
Luke Wood <lukewoodcs@gmail.com>
2017-11-26 00:36:08 -0800

    config stuff

--- mix.exs
+++ mix.exs
@@ -20,7 +20,14 @@ defmodule Umbrella.Mixfile do
 
   defp deps do
     [
-      {:uuid,       "~> 1.1" }
+      {:uuid,       "~> 1.1" },
+      {:phoenix, "~> 1.3.0"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_html, "~> 2.10"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:distillery, "~> 1.5", runtime: false},
     ]
   end
 end
--- modules/client/mix.exs
+++ modules/client/mix.exs
@@ -8,9 +8,11 @@ defmodule Client.Mixfile do
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
-      deps: deps()
     ]
   end
 
@@ -26,24 +28,4 @@ defmodule Client.Mixfile do
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
-
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
-  defp deps do
-    [
-      {:phoenix, "~> 1.3.0"},
-      {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_html, "~> 2.10"},
-      {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
-      {:distillery, "~> 1.5", runtime: false},
-      #my deps
-      {:world,  in_umbrella: true},
-      {:player, in_umbrella: true},
-      {:bullet, in_umbrella: true},
-      {:peek,   in_umbrella: true}
-    ]
-  end
 end
