727c4d6686e2d021927983d68cc86170300e717a
Luke Wood <lukewoodcs@gmail.com>
2017-11-26 16:40:38 -0800

    dope

--- config/config.exs
+++ config/config.exs
@@ -10,8 +10,8 @@ config :player,
   reload: 10
 
 config :world,
-  width:  1000,
-  height: 1000,
+  width:  750,
+  height: 750,
   interval: 10
 
 import_config("../modules/client/config/config.exs")
